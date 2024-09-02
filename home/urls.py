10c892ce9dbdcfa5d73fe754872652ee0709bb72
Jose-Reyes <jreyes@gohives.com>
2024-09-01 22:20:56 -0600

    actualizacion de invitacion

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('invitacion', views.invite_view, name='invite'),
 ]
---
+++ static/assets/js/settings.js
@@ -0,0 +1,6 @@
+"use strict";
+//$('body').attr('data-background-color', 'bg3');
+
+$(window).resize(function() {
+	$(window).width();
+});
